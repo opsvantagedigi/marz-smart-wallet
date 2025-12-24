7cf001ddb2cf78796ff653a82092cc209a898fbc
opsvantagedigi <opsvantagedigital@gmail.com>
2025-12-24 00:06:15 +0000

    Fix Tailwind config and purge issues

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        orbitron: ["Orbitron", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
